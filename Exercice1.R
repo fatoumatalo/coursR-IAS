8c8c16c3c17f922bcbef4e64b9683325eca46e98
fatoumatalo <matalo.fl70@gmail.com>
2022-01-26 22:45:09 +0100

    my first comit

---
+++ Exercice1.R
@@ -0,0 +1,12 @@
+rm(list=ls())
+
+nom_vecteur=c(1,2,0.5,'mon_nom')
+  #type de mon vecteur
+
+
+typeof(nom_vecteur)
+nom_vecteur
+#un vecteur nommé logique qui contient les elements suivants TRUE FALSE TRUE
+logique=c(TRUE,FALSE,T)
+logique
+sum(logique)
